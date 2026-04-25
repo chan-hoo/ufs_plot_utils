22c94941d639cb271bb923410edee0fbaae226d1
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-04-25 17:37:27 -0500

    fix test failure

--- tests/test_data_reader.py
+++ tests/test_data_reader.py
@@ -46,10 +46,61 @@ def test_slice_z_dimension():
     assert da.shape == (10, 10), f'''Expected shape (10, 10), got {da.shape}'''
 
 
-@pytest.mark.parametrize("time_idx,z_idx", [(0, None), (1, None), (None, 0), (None, 2)])
-def test_slice_various_indices(time_idx, z_idx):
+def test_slice_time_only_from_4d():
     """
-    Test slicing with various index combinations
+    Test slicing only time dimension from 4D array
+    """
+    ds = xr.Dataset({
+        "var": (("time", "z", "y", "x"), np.random.rand(3, 5, 10, 10))
+    })
+
+    reader = DataReader(DummyDataset())
+    da = reader._slice_data(ds["var"], None, 0)
+
+    assert "time" not in da.dims, f'''Time dimension should be removed, dims: {da.dims}'''
+    assert "z" in da.dims, f'''Z dimension should be preserved, dims: {da.dims}'''
+    assert da.ndim == 3, f'''Expected 3D result (z, y, x), got {da.ndim}D with shape {da.shape}'''
+    assert da.shape == (5, 10, 10), f'''Expected shape (5, 10, 10), got {da.shape}'''
+
+
+def test_slice_z_only_from_4d():
+    """
+    Test slicing only z dimension from 4D array
+    """
+    ds = xr.Dataset({
+        "var": (("time", "z", "y", "x"), np.random.rand(3, 5, 10, 10))
+    })
+
+    reader = DataReader(DummyDataset())
+    da = reader._slice_data(ds["var"], 2, None)
+
+    assert "z" not in da.dims, f'''Z dimension should be removed, dims: {da.dims}'''
+    assert "time" in da.dims, f'''Time dimension should be preserved, dims: {da.dims}'''
+    assert da.ndim == 3, f'''Expected 3D result (time, y, x), got {da.ndim}D with shape {da.shape}'''
+    assert da.shape == (3, 10, 10), f''''Expected shape (3, 10, 10), got {da.shape}'''
+
+
+def test_slice_both_time_and_z_from_4d():
+    """
+    Test slicing both time and z dimensions from 4D array
+    """
+    ds = xr.Dataset({
+        "var": (("time", "z", "y", "x"), np.random.rand(3, 5, 10, 10))
+    })
+
+    reader = DataReader(DummyDataset())
+    da = reader._slice_data(ds["var"], 1, 2)
+
+    assert "time" not in da.dims, f'''Time dimension should be removed, dims: {da.dims}'''
+    assert "z" not in da.dims, f'''Z dimension should be removed, dims: {da.dims}'''
+    assert da.ndim == 2, f'''Expected 2D result (y, x), got {da.ndim}D with shape {da.shape}'''
+    assert da.shape == (10, 10), f'''Expected shape (10, 10), got {da.shape}'''
+
+
+@pytest.mark.parametrize("time_idx,z_idx,expected_ndim", [(0, 1, 2), (1, 0, 2), (None, 0, 3), (0, None, 3)])
+def test_slice_parametrized(time_idx, z_idx, expected_ndim):
+    """
+    Test slicing with various index combinations and verify expected dimensionality
     """
     ds = xr.Dataset({
         "var": (("time", "z", "y", "x"), np.random.rand(3, 5, 10, 10))
@@ -58,5 +109,4 @@ def test_slice_various_indices(time_idx, z_idx):
     reader = DataReader(DummyDataset())
     da = reader._slice_data(ds["var"], z_idx, time_idx)
 
-    assert da.ndim == 2, f'''Expected 2D result, got {da.ndim}D with shape {da.shape}'''
-
+    assert da.ndim == expected_ndim, f'''Expected {expected_ndim}D result, got {da.ndim}D with shape {da.shape}'''
